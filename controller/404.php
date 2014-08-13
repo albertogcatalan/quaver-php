b7d5d07ac08587d978b58b82bb1da25cf6fb2be2
Alberto González <albertogonzcat@MacBook-Pro-de-Alberto.local>
2014-08-13 13:44:21 +0200

    Fix 404 controller

--- controller/404.php
+++ controller/404.php
@@ -10,7 +10,7 @@ $url = $this->getUrl();
 header("HTTP/1.0 404 Not Found");
 trigger_error("[404] $url", E_USER_WARNING);
 
-$template = $twig->loadTemplate("404.twig");
+$template = $this->twig->loadTemplate("404.twig");
 echo $template->render($this->twigVars);
 
 ?>
